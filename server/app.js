09e0e3805e90ed2a92246927562ae59e236e5463
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-28 23:24:07 +0900

    Feat : add Eslint, prettier
    - 코드의 가독성과 일관성을 위해 eslint 및 프리티어 적용

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.sendStatus(err.status || 500);
 });
 
