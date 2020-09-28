cb3a236cfc95811250c0c7189e8b38699a8a896a
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-28 21:04:49 +0900

    Feat : initialize environment

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.sendStatus(err.status || 500);
+});
+
+module.exports = app;
