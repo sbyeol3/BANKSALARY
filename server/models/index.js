e3121c3e517694bf11685fc6483809ec41114a57
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-01 01:06:12 +0900

    Feat : install modules and add db index file (#3)
    - mysql2, passport 관련 모듈 설치
    - dotenv를 이용하여 db connection 관련 코드 작성

---
+++ server/models/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+module.exports = pool;
