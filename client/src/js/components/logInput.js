fbfa1818669a6b1c139cd0737c697bbc2a1c1ef5
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-12 22:17:02 +0900

    Feat : add functions changing options by selecting kind (#10)
    - 수입/지출 선택 시 카테고리와 결제수단을 다르게 display

--- client/src/js/components/logInput.js
+++ client/src/js/components/logInput.js
@@ -10,6 +10,12 @@ import request from '../util/api';
 class LogInput {
   constructor(parentElement) {
     this.parentElement = parentElement;
+    this.selectOptions = {
+      in: '',
+      out: '',
+      pay: '',
+      default: '<option selected disabled>선택하세요</option>',
+    };
     this.addEvent(this.parentElement);
     this.getInCategories();
     this.getOutCategories();
@@ -17,13 +23,60 @@ class LogInput {
   }
 
   addEvent(parentElement) {
-    parentElement.addEventListener('change', this.onChange);
-    parentElement.addEventListener('change', this.onClick);
+    parentElement.addEventListener('change', this.onChange.bind(this));
+    parentElement.addEventListener('change', this.onClick.bind(this));
   }
 
-  onChange(e) {}
+  onChange(e) {
+    const { target } = e;
+    if (target.name === 'kind') {
+      const value = +target.value;
+      this.changeCtgOptions(value);
+      this.changePayOptions(value);
+    }
+  }
 
   onClick(e) {}
+
+  addOptions(type) {
+    if (type === 'IN') {
+      const { in: inCategories } = store.select.category;
+      const options = inCategories.reduce((prev, category) => {
+        const { code, title } = category;
+        const element = `<option value=${code}>${title}</option>`;
+        return prev + element;
+      }, '<option selected disabled>선택하세요</option>');
+      this.selectOptions.in = options;
+    } else if (type === 'OUT') {
+      const { out: outCategories } = store.select.category;
+      const options = outCategories.reduce((prev, category) => {
+        const { code, title } = category;
+        const element = `<option value=${code}>${title}</option>`;
+        return prev + element;
+      }, '<option selected disabled>선택하세요</option>');
+      this.selectOptions.out = options;
+    } else {
+      const { payment: payments } = store.select;
+      const options = payments.reduce((prev, payment) => {
+        const { code, title } = payment;
+        const element = `<option value=${code}>${title}</option>`;
+        return prev + element;
+      }, '<option selected disabled>선택하세요</option>');
+      this.selectOptions.pay = options;
+    }
+  }
+
+  changeCtgOptions(kind) {
+    const selectElement = document.getElementById('ctg-select');
+    const type = kind ? 'in' : 'out';
+    selectElement.innerHTML = this.selectOptions[type];
+  }
+
+  changePayOptions(kind) {
+    const selectElement = document.getElementById('pay-select');
+    const type = kind ? 'default' : 'pay';
+    selectElement.innerHTML = this.selectOptions[type];
+  }
 
   async getInCategories() {
     const config = {
@@ -35,6 +88,7 @@ class LogInput {
       const { data } = body;
       console.log(data);
       reducer(setInCategories(data));
+      this.addOptions('IN');
     }
   }
 
@@ -48,6 +102,7 @@ class LogInput {
       const { data } = body;
       console.log(data);
       reducer(setOutCategories(data));
+      this.addOptions('OUT');
     }
   }
 
@@ -61,6 +116,7 @@ class LogInput {
       const { data } = body;
       console.log(data);
       reducer(setPayments(data));
+      this.addOptions('PAY');
     }
   }
 
@@ -70,19 +126,19 @@ class LogInput {
             <form id='log-form' class='log-form'>
                 <fieldset class='inputs radios'>
                     <label class='label' for='kind'>분류</label>
-                    <input type='radio' name='kind' class='radio-input'>수입
-                    <input type='radio' name='kind' class='radio-input'>지출
+                    <input type='radio' name='kind' value=1 class='radio-input' selected >수입
+                    <input type='radio' name='kind' value=0 class='radio-input'>지출
                 </fieldset>
                 <fieldset class='inputs'>
                     <label class='label'>날짜</label>
                     <input type='text' name='logDate' class='txt-input'>
                     <label class='label' for='category'>카테고리</label>
-                    <select nmae='category' class='select'>
-                        <option>선택하세요</option>
+                    <select id='ctg-select' name='category' class='select'>
+                        <option selected disabled>선택하세요</option>
                     </select>
                     <label class='label' for='payment'>결제수단</label>
-                    <select nmae='payment' class='select'>
-                        <option>선택하세요</option>
+                    <select id='pay-select' name='payment' class='select'>
+                        <option selected disabled>선택하세요</option>
                     </select>
                 </fieldset>
                 <fieldset class='inputs'>
